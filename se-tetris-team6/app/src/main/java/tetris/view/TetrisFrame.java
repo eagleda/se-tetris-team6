e8a9f3be2433371b921b8c4fe21f0ff77fb44102
codn0185 <codn0185@gmail.com>
2025-11-12 23:14:16 +0900

    버튼 addActionListener 형태 설정

--- se-tetris-team6/app/src/main/java/tetris/view/TetrisFrame.java
+++ se-tetris-team6/app/src/main/java/tetris/view/TetrisFrame.java
@@ -12,6 +12,7 @@ import java.util.Objects;
 import javax.swing.AbstractAction;
 import javax.swing.ActionMap;
 import javax.swing.InputMap;
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLayeredPane;
@@ -161,6 +162,27 @@ public class TetrisFrame extends JFrame {
             displayPanel(gameLayout);
             gameController.startItemGame();
         });
+
+        mainPanel.localMultiNormalButton.addActionListener(e -> {
+            //
+        });
+        mainPanel.localMultiItemButton.addActionListener(e -> {
+            //
+        });
+        mainPanel.localMultiTimeLimitButton.addActionListener(e -> {
+            //
+        });
+
+        mainPanel.onlineMultiNormalButton.addActionListener(e -> {
+            //
+        });
+        mainPanel.onlineMultiItemButton.addActionListener(e -> {
+            //
+        });
+        mainPanel.onlineMultiTimeLimitButton.addActionListener(e -> {
+            //
+        });
+
         mainPanel.settingButton.addActionListener(e -> {
             displayPanel(settingPanel);
         });
