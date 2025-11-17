6437efb40997060cac809d696b67579508d6757d
codn0185 <codn0185@gmail.com>
2025-11-17 19:19:40 +0900

    주석 수정 및 필요없는 멤버 변수 제거, 메인 메뉴에서 ESC 키 입력 방지

--- se-tetris-team6/app/src/main/java/tetris/view/TetrisFrame.java
+++ se-tetris-team6/app/src/main/java/tetris/view/TetrisFrame.java
@@ -333,7 +333,7 @@ public class TetrisFrame extends JFrame {
                     gameModel.pauseGame();
                 } else if (state == GameState.PAUSED) {
                     gameModel.resumeGame();
-                } else {
+                } else if (state != GameState.MENU) {
                     displayPanel(prevPanel);
                 }
             }
