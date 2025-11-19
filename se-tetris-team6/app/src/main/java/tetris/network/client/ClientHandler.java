3479cce473d5fff71b0558bbcdf06d6c33e44827
largeblueberry <onlym3480@naver.com>
2025-11-19 19:06:14 +0900

    [feat] : 파일 구조 정리 및 각각의 역할 주석으로 작성 - cocurrent 폴더는 비동기 처리 역할을 할 파일들 모음 - network 폴더는 통신 역할을 할 파일들 모음

---
+++ se-tetris-team6/app/src/main/java/tetris/network/client/ClientHandler.java
@@ -0,0 +1,12 @@
+package tetris.network.client;
+
+public class ClientHandler {
+    
+}
+/**
+ * 클라이언트에서 서버와의 통신을 담당
+ * - 서버로부터 메시지 수신 및 처리
+ * - 서버에게 메시지 전송
+ * - 네트워크 예외 상황 처리
+ * - 지연시간 측정 및 연결 품질 모니터링
+ */
